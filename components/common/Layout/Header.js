4e40d58f46e4383211ff11bd399be4a31b05ed4e
Zainchohan6198 <86037420+ZainChohan6198@users.noreply.github.com>
2023-07-25 15:49:51 +0500

    error removed

--- components/common/Layout/Header.js
+++ components/common/Layout/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <div className="bg-white py-10">
-      <div className="flex gap-10 items-center"></div>
+      <div className="flex gap-10 items-center">Header</div>
     </div>
   );
 }
